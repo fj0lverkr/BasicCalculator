c3ea6b8ed16cef9cb58b6af919318a172a8573b4
nils <nils.nahooy@live.com>
2022-08-26 16:16:55 +0200

    new version of code, still some issues present.

--- app/src/main/java/com/nilsnahooy/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/nilsnahooy/basiccalculator/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.LinearLayoutCompat
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    private var inputStringList: MutableList<String> = mutableListOf()
     private var inputString: String? = null
     private var result: Double? = null
     private var resultTV: TextView? = null
@@ -36,40 +37,43 @@ class MainActivity : AppCompatActivity() {
         val btn = v as Button
         when(btn.text){
             "CLR" -> {
-                inputString = ""
+                inputStringList = mutableListOf()
                 result = 0.0
-                resultTV?.text = inputString
+                inputString = null
+                resultTV?.text = ""
             }
             "+", "-", "X", "/" -> {
                 if (operator != null) {
-                    val op: String = operator as String
-                    result = calculate(result, inputString, op)
+                    inputStringList.add(2, inputString as String)
+                    result = calculate(inputStringList)
+                    inputStringList = mutableListOf()
+                    inputStringList.add(0, result.toString())
+                    inputStringList.add(1, btn.text as String)
                     resultTV?.text = result?.toString() ?: "divide by zero error"
                 }else{
-                    result = inputString?.toDoubleOrNull()
-                    inputString = ""
+                    inputStringList.add(0,inputString as String)
+                    inputStringList.add(1, btn.text as String)
+                    inputString = null
+                    resultTV?.text = operator
                 }
                 operator = btn.text as String
-                //inputString = operator
             }
             "=" -> {
                 if (operator != null) {
-                    val op: String = operator as String
-                    result = calculate(result, inputString, op)
-                    inputString = ""
+                    inputStringList.add(2, inputString as String)
+                    result = calculate(inputStringList)
+                    inputString = null
+                    inputStringList = mutableListOf()
+                    inputStringList.add(0, result.toString())
                     resultTV?.text = result?.toString() ?: "divide by zero error"
                 } else {
                     result = inputString?.toDoubleOrNull()
                     resultTV?.text = inputString ?: "0.0"
-                    inputString = ""
                 }
             }
             else -> {
-                inputString = if((inputString == null || inputString == "")
-                    && !btn.text.contains("([\\-+/X])+")) {
+                inputString = if(inputString == null || inputString == "") {
                     btn.text as String?
-                } else if (btn.text.contains("([\\-+/X])+")) {
-                    inputString
                 } else {
                     "$inputString${btn.text as String}"
                 }
@@ -78,26 +82,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun calculate(result: Double?, input: String?, operator: String): Double? {
-        var prev: Double? = result
-        var inputDouble: Double? = input?.toDoubleOrNull()
-        if (inputDouble == null){
-           inputDouble = 0.0
-        }
-        if (prev == null) {
-            prev = 0.0
-        }
-        return when(operator) {
-            "+" -> prev + inputDouble
-            "-" -> prev - inputDouble
-            "/" -> {
-                if(inputDouble != 0.0){
-                    prev/inputDouble
-                }else{
-                    null
-                }
+    private fun calculate(input: List<String>): Double? {
+        if (input.size == 3) {
+            var firstNumber: Double? = input[0].toDoubleOrNull()
+            var secondNumber: Double? = input[2].toDoubleOrNull()
+            val operator: String = input[1]
+            if (firstNumber == null){
+                firstNumber = 0.0
             }
-            else -> prev * inputDouble
+            if (secondNumber == null) {
+                secondNumber = 0.0
+            }
+            return when(operator) {
+                "+" -> firstNumber + secondNumber
+                "-" -> firstNumber - secondNumber
+                "/" -> {
+                    if(secondNumber != 0.0){
+                        firstNumber/secondNumber
+                    }else{
+                        null
+                    }
+                }
+                else -> firstNumber * secondNumber
+            }
+        } else {
+            return null
         }
     }
 
